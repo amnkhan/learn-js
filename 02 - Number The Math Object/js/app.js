92fa74e7a693c7f708625552a8fa3f94411bcc8a
Al Amin Khan <amnkhan.me@gmail.com>
2018-08-14 22:32:13 +0600

    JavaScript Math Objects Learned

---
+++ 02 - Number The Math Object/js/app.js
@@ -0,0 +1,20 @@
+const num1 = 200;
+const num2 = 50;
+let val;
+
+// Math Objects
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.5)
+val = Math.ceil(2.3)
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-1);
+val = Math.pow(8 , 2);
+val = Math.min(2,100,66,55,1);
+val = Math.max(2,100,66,55,1);
+val = Math.random();
+val = Math.floor(Math.random() * 10 + 1);
+
+
+console.log(val);
